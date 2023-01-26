21abbb20494e728fe4acc1efbb1e42c53808ee43
Serhii <sdalin777@gmail.com>
2023-01-26 19:36:42 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj1 = {
+//     a:50
+// };
+
+// obj1.a = 10;
+// console.log(obj1);
+
+// var name = 'Serg';
+
+// {
+//     let result = 50;
+//     console.log(result);
+// }
+
+// const obj = {
+//     name: 'Alex',
+//     age: 31,
+//     isСrush: false
+// };
+
+// console.log(obj.isСrush);
+
+// let priority = 'high';
+
+// const obj2 = {
+//     low: 'color 1',
+//     'high': 'color 2',
+//     critical: 'color 3'
+// };
+
+// console.log(obj2.critical);
+// console.log(obj2[priority]); // obj.high
+
+// let arr = [1, 2, 3, 5, 6, 8, 9];
+// arr.length = 2;
+// console.log(arr);
+
+// const result2 = confirm("Are u eblan?");
+// console.log(result2);
+
+// const answer = prompt("What about quantity of cats in your home&", "999");
+// console.log(typeof(answer));
+
+// const answer = prompt('how old are you');
+
+// // alert(`you are really ${answer} years old`);
+
+// alert('you are really'+ ' ' + answer);
+
+const name = "Popkin";
+
+console.log(alert(`Hello how your ${name}`));
+
+const age = 20;
+
+let baba = 5;
