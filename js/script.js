fac994626978a00119ccfd899cfcc299f4cb996f
Serhii <sdalin777@gmail.com>
2023-02-07 19:09:38 +0200

    add 2 functions with new quenstion

--- js/script.js
+++ js/script.js
@@ -19,31 +19,55 @@ const personalMovieDB = {
   genres: [],
   privat: false,
 };
-for (let i = 1; i <= 2; i++) {
-  const lala = prompt("Один из последних просмотренных фильмов?");
-  const baba = prompt("На сколько оцените его?");
-  if (
-    lala.length != null &&
-    baba != null &&
-    lala != "" &&
-    baba != "" &&
-    lala.length < 50
-  ) {
-    personalMovieDB.movies[lala] = baba;
-    console.log("Done");
-  } else {
-    console.log("error");
-    i--;
+
+function rememberMyFilms() {
+  for (let i = 1; i <= 2; i++) {
+    const lala = prompt("Один из последних просмотренных фильмов?");
+    const baba = prompt("На сколько оцените его?");
+    if (
+      lala.length != null &&
+      baba != null &&
+      lala != "" &&
+      baba != "" &&
+      lala.length < 50
+    ) {
+      personalMovieDB.movies[lala] = baba;
+      console.log("Done");
+    } else {
+      console.log("error");
+      i--;
+    }
   }
 }
 
-if (personalMovieDB.count < 10) {
-  console.log("Вы отличник");
-} else if (personalMovieDB.count > 10 && personalMovieDB.count > 30) {
-  console.log("Вы сериальщик");
-} else if (personalMovieDB.count > 30) {
-  console.log("Вы помешанный");
-} else {
-  console.log("Произошла ошибка");
+rememberMyFilms();
+
+function detectMyLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("Вы отличник");
+  } else if (personalMovieDB.count > 10 && personalMovieDB.count > 30) {
+    console.log("Вы сериальщик");
+  } else if (personalMovieDB.count > 30) {
+    console.log("Вы помешанный");
+  } else {
+    console.log("Произошла ошибка");
+  }
 }
-console.log(personalMovieDB);
+
+detectMyLevel();
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for (let i = 0; i < 3; i++) {
+    personalMovieDB.genres[i] = prompt(
+      `Ваш любимый номер жанру под номером ${i + 1}`
+    );
+  }
+}
+writeYourGenres();
